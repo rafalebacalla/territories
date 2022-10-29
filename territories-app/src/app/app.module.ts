48047269695293dffbf39145cecbb42b113deba9
rafalebacalla <raprap.bacalla@gmail.com>
2022-10-30 00:41:41 +0800

    add validation for login form

--- territories-app/src/app/app.module.ts
+++ territories-app/src/app/app.module.ts
@@ -4,18 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatToolbarModule } from '@angular/material/toolbar'
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { LoginComponent } from './components/login/login.component';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent
-  ],
+  declarations: [AppComponent, LoginComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -25,8 +24,10 @@ import { LoginComponent } from './components/login/login.component';
     MatIconModule,
     MatFormFieldModule,
     MatInputModule,
+    ReactiveFormsModule,
+    MatSnackBarModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- territories-app/src/app/components/login/login.component.ts
+++ territories-app/src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-login',
@@ -7,15 +8,78 @@ import { FormGroup, FormControl } from '@angular/forms';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+  loginForm : any | undefined = undefined;
+  errorMessage : string  = '';
 
-  loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
-  })
+  loginValidation = {
+    username: [
+      { type: 'required', message: 'Username is required' },
+    ],
+    password: [
+      { type: 'required', message: 'Password is required' },
+    ]
+  };
 
-  constructor() { }
+  constructor(private snackBar: MatSnackBar) { }
 
   ngOnInit(): void {
+   this.loginForm = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    })
   }
 
+  setError(errorCode : string) : string {
+    let errorMessage : any = {}
+    switch(errorCode) {
+        case "incorrect-details":
+          this.loginForm.controls['password'].setErrors({'incorrect': true});
+          this.loginForm.controls['username'].setErrors({'incorrect': true});
+          break;
+        default:
+          this.loginForm.controls['username'].setErrors({'incorrect': true});
+          this.loginForm.controls['password'].setErrors({'incorrect': true});
+      }
+    return errorMessage;
+  }
+
+  async submitLoginForm(){
+    if(this.loginForm.status === 'INVALID') {
+      this.snackBar.open('Invalid details');
+      return;
+    }
+
+    let username = this.loginForm.controls.username.value;
+    let password = this.loginForm.controls.password.value;
+
+    const result : any = this.callDummyApi(username, password)
+    
+    if(!!result.error){
+      this.setError(result.error.code)
+      this.snackBar.open('Login Failed');
+    } else {
+      this.snackBar.open('Login Successful');
+    }
+    
+  }
+
+  callDummyApi (username : string, password : string) : any {
+    if (username === "user" && password === "1234") {
+      return {
+        result: {
+          status: "OK",
+          code: 200,
+          message: "Login successful"
+        }
+      }
+    } else {
+      return {
+        error: {
+          status: "Unauthenticated",
+          code: 401,
+          message: "Incorrect credentials"
+        }
+      }
+    }
+  }
 }
