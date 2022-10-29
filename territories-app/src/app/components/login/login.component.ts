816ff27e7e9fb158bad9226e2b7f35ec4a23cb39
rafalebacalla <raprap.bacalla@gmail.com>
2022-10-29 23:59:33 +0800

    create login form

--- territories-app/src/app/components/login/login.component.ts
+++ territories-app/src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.sass']
 })
 export class LoginComponent implements OnInit {
+
+  loginForm = new FormGroup({
+    username: new FormControl(''),
+    password: new FormControl(''),
+  })
 
   constructor() { }
 
